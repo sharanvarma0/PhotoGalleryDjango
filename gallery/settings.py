a6660e52a1df6d9ca5980737130babdd5f063f70
Sharan Varma <sharanvarma0@gmail.com>
2021-05-21 22:10:32 +0530

    Removed secret key string from settings

--- gallery/settings.py
+++ gallery/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'wx1#+kv6uht)!g2-++3=y2$&2#qkwsp!vy-@bn+m_85t_v1_1s'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
